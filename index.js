6829c84a247b63f3cf23b9dc93c7f102f443e51c
rodolv1979 <rodolfo.marraffa@libero.it>
2017-10-13 13:37:42 +0200

    Added CardioDay two (7th Project)

--- index.js
+++ index.js
@@ -104,3 +104,60 @@ const transportation = data.reduce(function(obj, item){
 },{});
 
 console.log(transportation);
+
+
+// ----------------------------------------------- ARRAY DAY 2 -------------------------------
+
+const people2 = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+];
+const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+date = new Date();
+
+const peopleOver19 = people2.some(el => {
+    return (date.getFullYear() - el.year) >= 19;
+});
+console.log(peopleOver19);
+
+// Array.prototype.every() // is everyone 19 or older?
+
+const allAdults = people2.every(el => {
+    return (date.getFullYear() - el.year) >= 19;
+});
+console.log(allAdults);
+
+// Array.prototype.find()
+
+// Find is like filter, but instead returns just the one you are looking for
+
+const comment = comments.find(comm => {
+    return (comm.id === 823423);
+});
+// find the comment with the ID of 823423
+console.log(comment);
+// Array.prototype.findIndex()
+
+// Find the comment with this ID
+
+// delete the comment with the ID of 823423
+
+const index = comments.findIndex(comment => comment.id === 823423);
+console.log(index);
+// comments.splice(index, 1);
+
+const newComments = [
+    ...comments.slice(0, index),
+    ...comments.slice(index + 1)
+];
